998d90173f7166cab11148c17f58d5cd44227158
Israel G. Lugo <israel.lugo@lugosys.com>
2016-06-07 03:36:46 +0100

    Add license information to main source file.

--- nonet.c
+++ nonet.c
@@ -1,3 +1,25 @@
+/*
+ * nonet - execute a command without network access
+ * Copyright (C) 2016 Israel G. Lugo
+ *
+ * This file is part of nonet.
+ *
+ * nonet is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * nonet is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with nonet. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For suggestions, feedback or bug reports: israel.lugo@lugosys.com
+ */
+
 #define _GNU_SOURCE
 #include <sched.h>
 
